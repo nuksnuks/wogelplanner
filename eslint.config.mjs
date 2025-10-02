8a923f0f646b407e8ce8424d9c3ca92d2858486c
nuksnuks <davidwogelius@gmail.com>
2025-10-02 04:29:45 +0200

    vercel deploy new 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,6 +18,8 @@ const eslintConfig = [
       "out/**",
       "build/**",
       "next-env.d.ts",
+      "src/app/dataconnect-generated/**",
+      "src/dataconnect-generated/**",
     ],
   },
 ];
